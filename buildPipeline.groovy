7045a52e370903034dbc35df5aff177d313ff14b
Anuj Sharma <anuj_sharma401@yahoo.com>
2023-10-07 11:42:21 +0530

    Create buildPipeline.groovy

---
+++ buildPipeline.groovy
@@ -0,0 +1,22 @@
+node(){
+    def sonarScanner = tool name: 'SonarScanner', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
+	def repoName = "https://github.com/anujdevopslearn/SonarQubeCoverageJava.git"
+	stage('Code Checkout'){
+		git changelog: false, credentialsId: 'GitHubCreds', poll: false, url: repoName
+	}
+	stage('Maven Build'){
+		sh """
+			ls -lart
+			date
+			mvn clean install
+		"""
+	}
+	stage('Code Review'){
+		withSonarQubeEnv(credentialsId: 'SonarQubeToken') {
+			sh "${sonarScanner}/bin/sonar-scanner"
+		}
+	}
+	stage('Code Deployment'){
+	
+	}
+}
